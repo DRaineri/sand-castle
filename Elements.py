217a7b662e362ef2398418926411aa9c6ca49b43
Dino Raineri <dino.raineri@insa-lyon.fr>
2014-01-18 14:19:28 +0100

    new super

--- Elements.py
+++ Elements.py
@@ -7,25 +7,24 @@ class Element(object):
 		self.state = Idle()
 
 class Creature(Element):
-	def __init__(self):
-		super(Monster, self).__init__()
+	def __init__(self, lp):
+		super(Creature, self).__init__()
+		this.lifePoint = lp
 		this.angle = 0;
 
 class StillObject(Element):
 	def __init__(self):
-		super(ClassName, self).__init__()
+		super(StillObject, self).__init__()
 		
 class Character(Creature):
 	def __init__(self, name):
+		super(Character, self).__init__()
 		self.name = name
 
 class Castle(Creature):
-	def __init__(self, arg):
-		super(Castle,Creature.__init__()
-		self.arg = arg
+	def __init__(self):
+		super(Castle,self).__init__()
 		
 class Monster(Creature):
 	def __init__(self, arg):
 		super(Monster, self).__init__()
-		self.arg = arg
-		
