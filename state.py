b80c66ec2eb891c8014d28c2b4b2f309546c4a30
Adrien Duffy <adrien.duffy-coissard@insa-lyon.fr>
2014-01-18 16:15:17 +0100

    state moving base done

--- state.py
+++ state.py
@@ -1,29 +1,68 @@
+import math
+
 class State(object):
 	"""docstring for State"""
+	def __init__(self, element):
+		self.t = 0
+		self.element = element		
+
+
+
+	def update(self, dt):
+		self.t += dt
+
+	
+
+class Idle(State):
+	"""docstring for Idle"""
 	def __init__(self, arg):
-			self.arg = arg
-		
+		super(Idle, self).__init__()
+		self.arg = arg
+
+
 class Moving(State):
 	"""docstring for Moving"""
-	def __init__(self, ):
-		super(Moving, self).__init__()
+
+	anim_delay = 0.2
+	def __init__(self, element, anim_delay=anim_delay):
+		super(Moving, self).__init__(element)
+		self.anim_delay = anim_delay
+
+
+	def update(self, dt):
+		super(Moving,self).update(dt)
+		angle = self.element
+		distancePix=self.element.speed*dt
+		self.element.pos.posX=distancePix*cos(angle)
+		self.element.pos.posY=distancePix*sin(angle)
+
+		imagesMoving = self.element.images[Moving]
+		imagesFrame=imagesMoving[(self.t / self.anim_delay) % len(self.element.images)]
+		self.element.cur_image=imagesFrame[ceil(angle/(2*PI)*len(imagesFrame))]	
 		
 
-class Attacking(State):
-	"""docstring for Attacking"""
-	def __init__(self, arg):
-		super(Attacking, self).__init__()
-		self.arg = arg
 
 
-class Iddle(State):
-	"""docstring for Iddle"""
-	def __init__(self, arg):
-		super(Iddle, self).__init__()
-		self.arg = arg
 		
-class Openning(State):
-	"""docstring for Openning"""
-	def __init__(self, arg):
-		super(Openning, self).__init__()
-		self.arg = arg
+ #
+ #class Attacking(State):
+ #	"""docstring for Attacking"""
+ #	def __init__(self, arg):
+ #		super(Attacking, self).__init__()
+ #		self.arg = arg
+ #
+ #	def 
+ #
+ #
+ #class Idle(State):
+ #	"""docstring for Iddle"""
+ #	def __init__(self, arg):
+ #		super(Iddle, self).__init__()
+ #		self.arg = arg
+ #		
+ #class Openning(State):
+ #	"""docstring for Openning"""
+ #	def __init__(self, arg):
+ #		super(Openning, self).__init__()
+ #		self.arg = arg
+ #
