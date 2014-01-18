83f4d1af1772255a365c3ece030b043c11fb51d8
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-01-18 15:48:04 +0100

    Implemented base logic for game and grid

--- game.py
+++ game.py
@@ -30,7 +30,9 @@ class GameWindow(pyglet.window.Window):
 
 
 	def update(self, dt):
-		pass
+		for element in self.elements:
+			element.update(dt)
+
 
 	def on_draw(self):
 		self.background.draw()
@@ -42,8 +44,8 @@ class GameWindow(pyglet.window.Window):
 	def on_mouse_motion(self, x, y, dx, dy):
 		pass
 
-	def on_mouse_press(self, x, y):
-		print x, y
+	def on_mouse_press(self, x, y, button, modifiers):
+		pass
 
 	def on_key_press(self, symbol, modifiers):
 		x_diff = 0
--- grid.py
+++ grid.py
@@ -1,4 +1,35 @@
 import random
+import config
+
+def random_bg():
+	return random.choice([Sand])
+
+class Grid(object):
+	def __init__(self, w, h):
+		self.grid = [[ None for x in xrange(w)] for y in xrange(h)]
+		self.random_populate()
+
+	def random_populate(self):
+		for y, row in enumerate(self.grid):
+			for x, col in enumerate(row):
+				r_x, r_y = x * config.CELL_SIZE, y * config.CELL_SIZE
+				self.grid[y][x] = Cell(background=Sand(r_x, r_y))
+
+	def draw_background(self):
+		for row in self.grid:
+			for cell in row:
+				cell.draw()
+
+
+class Cell(object):
+	def __init__(self, background, element=None):
+		self.background = background
+		self.element = element
+
+	def draw(self):
+		self.background.draw()
+		if self.element:
+			self.element.draw()
 
 class Background(object):
 	"""Class that generate a background object"""
